d89bc00aba408917e79d6c68992d34ca6f1a4370
Ricardo Palma <palmamendoza.5@hotmail.com>
2019-02-12 13:38:47 -0600

    added main program

---
+++ labs/lab1.1/mycat.c
@@ -0,0 +1,36 @@
+// Ricardo Palma A01226922
+/* Sources:
+ *			http://codewiki.wikidot.com/c:system-calls:open 
+ * 		http://codewiki.wikidot.com/c:system-calls:read
+ * 		http://codewiki.wikidot.com/c:system-calls:write
+ * 		http://codewiki.wikidot.com/c:system-calls:close
+ */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+#define SIZE_BUF 128
+
+int main(int argc, char *argv[]){
+
+	int file;
+	file = open(argv[1], O_RDONLY);	
+	char buf[SIZE_BUF]; 	// A character array where the read content will be stored.
+	
+	// /cygdrive/c/Users/Ricardo Palma/Documents/7mo Semestre/Advanced Programming/Lab1.1
+
+	if(file < 0){
+		write(2, "Error: Couldn't read file.\n'", SIZE_BUF);
+	}
+	while(read(file, buf, SIZE_BUF -1) != 0){
+			write(1, buf, SIZE_BUF);
+			memset(buf, '\0', SIZE_BUF);
+	}
+
+	close(file);
+
+}
