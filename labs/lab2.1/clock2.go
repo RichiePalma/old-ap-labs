130bc2f99e41b574a6faca0ad46bd54d80b962cf
Ricardo Palma <palmamendoza.5@hotmail.com>
2019-02-26 21:37:42 -0600

    added clock2

---
+++ labs/lab2.1/clock2.go
@@ -0,0 +1,46 @@
+// Clock2 is a concurrent TCP server that periodically writes the time.
+
+/* Sources:
+			http://www.golangprograms.com/subtract-n-number-of-year-month-day-hour-minute-second-millisecond-microsecond-and-nanosecond-to-current-date-time.html
+			https://gobyexample.com/command-line-flags	
+ */		
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+	"os"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		 //io.WriteString(c, "Mexico: ")
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func main() {
+	args := os.Args
+	
+	listener, err :=  net.Listen("tcp", args[1]) //test localhost:9090
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err :=  listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+
+		go handleConn(conn) // handle connections concurrently
+	}
+}
